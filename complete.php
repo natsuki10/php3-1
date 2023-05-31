879e9421407d8547e2d3fa6ba76ca56ae3a6afa8
natsuki <natsuki.uji410@gmail.com>
2023-05-31 23:09:22 +0900

    課題3-1完了しました

---
+++ complete.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="style.css">
+<html>
+<head>
+    <title>投稿完了</title>
+</head>
+<body>
+    <h1>投稿完了</h1>
+    <p>投稿が完了しました。</p>
+    <a href="index.php" class="button-link">投稿画面に戻る</a>
+</body>
+</html>
